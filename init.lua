d59136becc3387d55b16e1c2bfbe077a2b890c64
Matheus Lessa <matheus@fastmail.com>
2020-10-16 12:44:38 -0300

    da pra executar um comando a partir de um prompt!

--- init.lua
+++ init.lua
@@ -8,5 +8,22 @@ require "langs.html"
 
 keymap.normal("<c-s>", ":s<enter>")
 
-function pipe()
+function pipe_from_input()
+	read_line.prompt("!")
+	read_line.read(function(input)
+		if input == nil then
+			return
+		end
+	
+		local iter = string.gmatch(input, "%S+")
+		local command = iter()
+		local args = {}
+		for a in iter do
+			args[#args + 1] = a
+		end
+		
+		local output = process.pipe(command, args)
+		print(output)
+	end)
 end
+keymap.normal("!", ":pipe_from_input()<enter>")
