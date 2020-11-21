e85855f5bc2517be7cc4b876894552a2fc1dc42a
Matheus Lessa <matheus@fastmail.com>
2020-11-20 21:50:09 -0300

    não precisa mais de hack pra copiar/colar

--- init.lua
+++ init.lua
@@ -1,22 +1,5 @@
 require "tools"
 
-function copy()
-	local selection = buffer_view.selection_text()
-	if #selection > 0 then
-		process.pipe("copycat", {"-i"}, selection)
-	end
-end
-keymap.normal("y", ":copy<enter>")
-
-function paste()
-	buffer_view.delete_selection()
-	local stdout, stderr, success = process.pipe("copycat", {"-o"})
-	if success and #stdout > 0 then
-		buffer_view.insert_text(stdout)
-	end
-end
-keymap.normal("Y", ":paste<enter>")
-
 langs = {}
 for i,ext in ipairs({"rs", "lua", "cs", "js", "html", "md"}) do
 	langs[#langs + 1] = {
--- langs/js.lua
+++ langs/js.lua
@@ -1,6 +1,6 @@
 syntax.rules("**/*.js", {
 	keyword = {"break", "export", "super", "case", "extends", "switch", "catch", "finally", "class", "for", "throw",
-		"const", "function", "try", "continue", "if", "typeof", "debugger", "import", "var", "default", "in", "void",
+		"const", "function", "try", "continue", "if", "typeof", "debugger", "import", "var", "default", "in", "of", "void",
 		"delete", "instanceof", "while", "do", "new", "with", "else", "return", "yield", "enum", "implements", "package",
 		"public", "interface", "private", "static", "let", "protected", "yield", "async", "await", "abstract", "float",
 		"synchronized", "boolean", "goto", "throws", "byte", "int", "transient", "char", "long", "volatile", "double",
