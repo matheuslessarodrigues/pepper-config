30a7d59f86e5dc39a54e7aca6acd49914aa95b4f
Matheus Lessa <1696072+matheuslessarodrigues@users.noreply.github.com>
2020-12-23 01:17:55 -0300

    new syntax definition

--- langs/lua.lua
+++ langs/lua.lua
@@ -1,10 +1,7 @@
 syntax.rules("**/*.lua", {
-	keyword = {"and", "break", "do", "else", "elseif", "end", "for", "function", "if", "in", "local", "not", "or",
-		"repeat", "return", "then", "until", "while"},
-	symbol = {"+", "-", "*", "/", "%%", "^", "#", "<", ">", "=", "~", "%(", "%)", "%{", "%}", "%[", "%]", ";",":",
-		",", "%.", "%.%.", "%.%.%."},
-	comment = {"--{.}", "--%[%[{!(%]%]).$}"},
-	literal = {"nil", "false", "true", "_G", "_ENV", "%d{%w%._}"},
-	string = {"'{(\\')!'.}", "\"{(\\\")!\".}", "%[%[{!(%]%]).}"},
-	text = {"%a{%w_}", "_{%w_}"},
+	keyword = "and|break|do|else|elseif|end|for|function|if|in|local|not|or|repeat|return|then|until|while",
+	symbol = "+|-|*|/|%%|^|#|<|>|=|~|%(|%)|%{|%}|%[|%]|;|:|,|%.|%.%.|%.%.%.",
+	literal = "nil|false|true|_G|_ENV|%d{%w%._}",
+	string = [['{(\\')!'.}|"{(\\")!".}|%[%[{!(%]%]).}]],
+	comment = "--{.}|--%[%[{!(%]%]).$}",
 })
