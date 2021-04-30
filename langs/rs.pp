88b677e9c54fa6889ab9245eb7f748e81976a880
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2021-04-30 17:41:42 -0300

    static no rs

--- langs/rs.pp
+++ langs/rs.pp
@@ -1,5 +1,5 @@
 syntax "**/*.rs" {
-	keywords = {fn|let|const|if|else|match|loop|while|for|break|continue|return|mod|use|as|in|enum|struct|trait|impl|where|mut|ref|pub|unsafe|extern}
+	keywords = {fn|let|const|static|if|else|match|loop|while|for|break|continue|return|mod|use|as|in|enum|struct|trait|impl|where|mut|ref|pub|unsafe|extern}
 	types = {bool|u8|u16|u32|u64|usize|i8|i16|i32|i64|isize|f32|f64|str|char|%u{%w_}}
 	symbols = {%(|%)|%[|%]|%{|%}|:|;|,|=|<|>|+|-|/|*|%%|%!|?|&|%||@}
 	literals = {true|false|self|'\.{!'.}|'.'|b'\.{!'.}|b'.'|%d{%w%._}|'%a{%w_}}
