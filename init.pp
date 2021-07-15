2fdf244f09a04599bccef3fb6f88f7060bf3c26d
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2021-07-15 17:29:06 -0300

    new config

---
+++ init.pp
@@ -0,0 +1,3 @@
+lsp "rust-analyzer" "**/*.rs" "rust-analyzer-log.txt"
+lsp "omnisharp -lsp --encoding utf-8" "**/*.cs" "omnisharp-log.txt"
+lsp "clangd --offset-encoding=utf-8" "**/*.{c,h,cpp,hpp}" "clangd-log.txt"
